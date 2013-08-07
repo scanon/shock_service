847ef3e44dc1d79cc04618a07b71b3116b0f010b
Jared Wilkening <jared.wilkening@gmail.com>
2013-08-07 15:55:18 -0400

    update makefile and install scripts

---
+++ install-libs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+
+TARGET=$1
+LIB_DIR=$TARGET/lib
+PERL=/kb/runtime/perl
+
+# prepare libs
+cd libs
+./prepare.sh
+
+# install perl Bio::KBase::Shock
+cd perl/Bio-KBase-Shock
+${PERL} Makefile.PL
+make 
+make install
+cd -
+
+# install python shock
+pip install python
+
+cd ..

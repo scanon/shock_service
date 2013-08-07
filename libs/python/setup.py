aa634465c65b2ceb170ab779a9de270e1d48418a
Jared Wilkening <jared.wilkening@gmail.com>
2013-08-07 15:40:17 -0400

    working perl and python installs

--- libs/python/setup.py
+++ libs/python/setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import biokbase.shock
+import shock
 
-setup(name='biokbase.shock',
-      version=biokbase.shock.__version__,
-      author=biokbase.shock.__author__,
-      license=biokbase.shock.__licence__,
-      packages=['biokbase.shock'],
+setup(name='shock',
+      version=shock.__version__,
+      author=shock.__author__,
+      license=shock.__licence__,
+      packages=['shock'],
      )

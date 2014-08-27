7c8cec445c143d26bee7b28b5f4031b38f2fb9c7
Jared bischof <jbischof@gmail.com>
2014-08-27 14:33:11 -0500

    Test edit

--- fix_dates.js
+++ fix_dates.js
@@ -1,4 +1,3 @@
-
 var cursor = db.Nodes.find()
 while (cursor.hasNext()) { var doc = cursor.next(); db.Nodes.update({_id: doc._id}, {$set: {last_modified : new Date(doc.last_modified)}}) }
 var cursor = db.Nodes.find()
